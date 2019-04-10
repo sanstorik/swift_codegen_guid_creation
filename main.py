a20d4f1ce5a5b7fb76449fba5621442d688913cd
Developers <developer1@weinand.it>
2019-04-10 16:27:26 +0300

    Created inner relationships processing

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import xml.etree.ElementTree as ET
+import inflect as Plural
 
 output = file("output.txt", "a+")
 
 def create_guid_extension(entity_xml):
     entity_name = entity_xml.get("name")
-    output.write("\nextension {} {} \n".format(entity_name, "{"))
-    output.write("    var syncEntity: SyncEntityType { return SyncEntity.%s }" % (entity_name + "s"))
+    plural_entity_name = Plural.engine().plural(entity_name)
+
+    output.write("\n\nextension {}: GUIDMapEntity {} \n".format(entity_name, "{"))
+    output.write("    var syncEntity: SyncEntityType { return SyncEntity.%s }" % (plural_entity_name))
     output.write("\n\n    func process(in context: NSManagedObjectContext, _ fetcher: GuidFetcher) {")
     output.write("\n\tappendSelf(in: context, fetcher)")
 
-    for inner_relationship in entity_xml.findall("relationship"):
-        inner_entity_name = inner_relationship.get("name")
-        output.write("\n\t{}?.appendSelf(in: context, fetcher)".format(inner_entity_name))
+    writeInnerRelationships(entity_xml)
 
     output.write("\n    }")
     output.write("\n}")
+
+
+def writeInnerRelationships(entity_xml):
+    inner_relationships = entity_xml.findall("relationship")
+    single_inner_relationships = filter(lambda x: x.get("toMany") == "NO", inner_relationships)
+    tomany_inner_relationships = filter(lambda x: x.get("toMany") == "YES", inner_relationships)
+
+    for inner_relationship in single_inner_relationships:
+        inner_entity_name = inner_relationship.get("name")
+        output.write("\n\t{}.appendSelf(in: context, fetcher)".format(inner_entity_name))
+
+    output.write("\n")
+    for inner_relationship in tomany_inner_relationships:
+        inner_entity_name = inner_relationship.get("name")
+        output.write("\n\tfor entity in %s {" % (inner_entity_name))
+        output.write("\n\t    entity?.appendSelf(in: context, fetcher)")
+        output.write("\n\t}")
+
 
 
 if __name__ == "__main__":
