eaa6c0556b5988cbffc5a24856dfb184727a4352
Developers <developer1@weinand.it>
2019-04-11 11:52:47 +0300

    Updated

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def create_guid_extension(entity_xml):
     output.write("\n\n    func process(in context: NSManagedObjectContext, _ fetcher: GuidFetcher) {")
     output.write("\n\tappendSelf(in: context, fetcher)")
 
-    writeInnerRelationships(entity_xml)
+    write_inner_relationships(entity_xml)
 
     output.write("\n    }")
     output.write("\n}")
 
 
-def writeInnerRelationships(entity_xml):
+def write_inner_relationships(entity_xml):
     inner_relationships = entity_xml.findall("relationship")
     single_inner_relationships = filter(lambda x: x.get("toMany") == "NO" or x.get("toMany") is None, inner_relationships)
     tomany_inner_relationships = filter(lambda x: x.get("toMany") == "YES", inner_relationships)
